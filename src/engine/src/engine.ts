d127713d9458ac339c1bf69e2563ab5b6d1a180a
Ilham Adiputra <ilhamadiputra4@gmail.com>
2026-04-03 14:45:54 +0700

    fix(engine): engine queue wrong logic

--- src/engine/src/engine.ts
+++ src/engine/src/engine.ts
@@ -7,17 +7,39 @@ import type { Workflow } from "./types/workflow.js";
 import { helpers } from "./utils/helpers.js";
 
 export async function main(json: Workflow, req: Request) {
-  const nodes = workflowToNodeTypes(json);
+  const pipeline = workflowToNodeTypes(json);
 
   const results = new Map<string, NodeExecutionData[][]>();
-  const queue = nodes;
-
-  while (queue.length) {
-    const node = queue.shift()!;
-    const input = collectInputs(node, results, req);
-    const output = await node.execute(input);
-    results.set(node.description.name, output);
+  const queue: NodeType[] = [];
+  const triggerNode = pipeline.find(
+    (node) => node.description.type === "trigger",
+  );
+  if (!triggerNode) {
+    throw new Error("No trigger node found");
   }
+
+  const nodeMap = pipeline.reduce((acc, node) => {
+    acc.set(node.description.name, node);
+    return acc;
+  }, new Map<string, NodeType>());
+
+  queue.push(triggerNode, ...getQueue(nodeMap, triggerNode));
+
+  console.log(
+    "queue",
+    JSON.stringify(
+      queue.map((node) => node.description.name),
+      null,
+      2,
+    ),
+  );
+
+  // while (queue.length) {
+  //   const node = queue.shift()!;
+  //   const input = collectInputs(node, results, req);
+  //   const output = await node.execute(input);
+  //   results.set(node.description.name, output);
+  // }
 }
 
 const collectInputs = (
@@ -78,4 +100,18 @@ const collectInputs = (
 
 function workflowToNodeTypes(workflow: Workflow): NodeType[] {
   return workflow.nodes.map((node) => nodes[node.type](workflow, node));
+}
+
+function getQueue(
+  nodeMap: Map<string, NodeType>,
+  currentNode: NodeType,
+): NodeType[] {
+  const queue: NodeType[] = [];
+
+  currentNode.description.output.forEach((output) => {
+    const node = nodeMap.get(output.toNode)!;
+    queue.push(node, ...getQueue(nodeMap, node));
+  });
+
+  return queue;
 }
